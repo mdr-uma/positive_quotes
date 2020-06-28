c184d2a6ca6569a885334b088b2686ba71661fb6
github username <um_2003_8@hotmail.com>
2020-06-27 22:54:35 -0400

    removed unwanted function and chaged variable name

--- frontend_js/src/index.js
+++ frontend_js/src/index.js
@@ -1,20 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     getCatagory();
-    getQuote();
 })
 
 function getCatagory() {
-    const categoryApi = new ApiService;
-    categoryApi.getCatagories("categories")
+    const api = new ApiService;
+    api.getCatagories("categories")
         .then(data => {
             data.forEach(d => {
                 const newCategory = new Category(d)
                 newCategory.categoryName
             })
         })
-}
-
-function getQuote() {
-    const quoteApi = new ApiService;
-
 }
