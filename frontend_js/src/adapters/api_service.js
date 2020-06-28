bb337752be3ee52ac74b8ad89b504372b747904f
github username <um_2003_8@hotmail.com>
2020-06-27 22:59:10 -0400

    created fetch request to create new quote

--- frontend_js/src/adapters/api_service.js
+++ frontend_js/src/adapters/api_service.js
@@ -12,4 +12,19 @@ class ApiService {
         return fetch(`${this.baseUrl}categories/${q}`)
             .then(res => res.json())
     }
+
+    fetchCreateNewQuote() {
+        return fetch(`${this.baseUrl}quotes`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify({
+                    phrase: event.target[0].value,
+                    categoryId: event.target[1].value
+                })
+            })
+            .then(res => res.json())
+    }
 }
