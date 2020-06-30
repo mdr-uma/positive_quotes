a2b11b22ba5494bd43e9974842b2c9f6622371c1
github username <um_2003_8@hotmail.com>
2020-06-29 22:20:18 -0400

    added condition to displayPhrase function

--- frontend_js/src/components/quote.js
+++ frontend_js/src/components/quote.js
@@ -4,9 +4,14 @@ class Quote {
         this.phrase = phrase
         this.categoryId = category_id
     }
-
-    displayPhrase(div) {
+    displayPhrase() {
         const quotes = document.querySelector(".quotes")
+        if (document.getElementById("inner-quotes-div")) {
+            quotes.removeChild(document.getElementById("inner-quotes-div"))
+        }
+
+        let div = document.createElement("div")
+        div.setAttribute("id", "inner-quotes-div")
         const p = document.createElement("p")
         p.innerText = `${this.phrase}`
         div.appendChild(p)
