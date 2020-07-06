b3c083e5c810a67ace7359206199d4082a1268a5
github username <um_2003_8@hotmail.com>
2020-07-05 20:37:02 -0400

    update validation

--- backend/app/models/quote.rb
+++ backend/app/models/quote.rb
@@ -1,4 +1,4 @@
 class Quote < ApplicationRecord
     belongs_to :category
-    validates :phrase, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters" }  
+    validates :phrase, presence: true, uniqueness: true 
 end
